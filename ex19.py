5f2ed77ae2d0b13b8e5caaab1ab3f1686d57ed2e
Dalal <DalalNS@legal.regn.net>
2019-11-19 21:11:24 +0530

    ex19.py
    ex19

---
+++ ex19.py
@@ -0,0 +1,9 @@
+def cheese_and_character(cheese_count,cracters_count):
+	print(f"The chess count is {cheese_count} and the crackers count is {cracters_count}")
+	
+cheese_and_character(10,20)
+
+cheese_count=100
+cracters_count=200
+
+cheese_and_character(cheese_count, cracters_count)
